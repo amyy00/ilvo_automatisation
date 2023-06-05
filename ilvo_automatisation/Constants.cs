ea8ff050582a223640b9b3b6033caed55f765c28
Jarne Staes <jarne.staes@student.vives.be>
2023-06-05 14:11:28 +0200

    added new connectionstring

---
+++ ilvo_automatisation/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace ilvo_automatisation
+{
+    internal class Constants
+    {
+        internal static string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=EMAV;trusted_connection=true;TrustServerCertificate=true;";
+
+        internal static string outputPath = "C:\\Users\\TheWi\\source\\repos\\ilvo_automatisation\\Database.cs"; // Replace with your desired output path
+    }
+}
