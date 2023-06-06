4fb9595faedac6969685dbcd98c591ba1006b0e7
Amy Norman <r0900514@student.vives.be>
2023-06-06 15:14:00 +0200

    added mock data

--- ilvo_automatisation/Program.cs
+++ ilvo_automatisation/Program.cs
@@ -6,19 +6,9 @@ public class Program
     public static void Main(string[] args)
     {
         GenerateMockData generateMockData;
-        //string connectionString = Constants.connectionString; // Replace with your database connection string
-        //string outputPath = GetDefaultOutputPath(); // Get the default output path
 
         //To fill the database with mock data
-        var dataGenerator = new GenerateMockData();
-        dataGenerator.GenerateData();
+        //var dataGenerator = new GenerateMockData();
+        //dataGenerator.GenerateData();
     }
-
-    //private static string GetDefaultOutputPath()
-    //{
-    //    string homeDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-    //    string defaultOutputPath = Path.Combine(homeDirectory, "DatabaseClasses");
-    //    Directory.CreateDirectory(defaultOutputPath);
-    //    return defaultOutputPath;
-    //}
 }
