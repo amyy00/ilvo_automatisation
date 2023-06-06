59c63ec41d4e7bc2e015b885adf1dd38dd35abc4
Jarne Staes <jarne.staes@student.vives.be>
2023-06-06 10:12:53 +0200

    added classes to dbcontext set

--- ilvo_automatisation/IlvoDbContext.cs
+++ ilvo_automatisation/IlvoDbContext.cs
@@ -1,13 +1,76 @@
 ﻿using ilvo_automatisation.Models;
 using Microsoft.EntityFrameworkCore;
 
+namespace ilvo_automatisation;
+
 public class IlvoDbContext : DbContext
 {
-    public DbSet<LnkGewassen> LnkGewassen { get; set; }
+    public DbSet<AangifteDierStalPas> AangifteDierStalPas { get; set; }
+    public DbSet<Bemesting> Bemestings { get; set; }
+    public DbSet<BuiVlaOpslverschil> BuiVlaOpslverschils { get; set; }
+    public DbSet<Burenregelingen> Burenregelingens { get; set; }
+    public DbSet<BurenregelingenEff> BurenregelingenEffs { get; set; }
+    public DbSet<BuTransportVracht> BuTransportVrachts { get; set; }
+    public DbSet<BuTransportVrachtEff> BuTransportVrachtEffs { get; set; }
+    public DbSet<CoSlib> CoSlibs { get; set; }
+    public DbSet<DierProd> DierProds { get; set; }
+    public DbSet<EmavContext> EmavContexts { get; set; }
+    public DbSet<EvoaAanbieder> EvoaAanbieders { get; set; }
+    public DbSet<Exploitatie> Exploitaties { get; set; }
+    public DbSet<GebruikKm> GebruikKms { get; set; }
+    public DbSet<GeoEmissieGemeente> GeoEmissieGemeentes { get; set; }
+    public DbSet<GeoEmissieGemeente2019> GeoEmissieGemeente2019s { get; set; }
+    public DbSet<GeoEmissieMestverwerking> GeoEmissieMestverwerkings { get; set; }
+    public DbSet<GeoEmissieVlops1000> GeoEmissieVlops1000s { get; set; }
+    public DbSet<GeoEmissieVlops250> GeoEmissieVlops250s { get; set; }
+    public DbSet<GeoExploitatie> GeoExploitaties { get; set; }
+    public DbSet<GeoMestverwerking> GeoMestverwerkings { get; set; }
+    public DbSet<GeometryColumn> GeometryColumns { get; set; }
+    public DbSet<GeoOpslag> GeoOpslags { get; set; }
+    public DbSet<GeoProvincie> GeoProvincies { get; set; }
+    public DbSet<GeoStal> GeoStals { get; set; }
+    public DbSet<GeoUitrijden> GeoUitrijdens { get; set; }
+    public DbSet<GeoWeide> GeoWeides { get; set; }
+    public DbSet<IdentExploitatie> IdentExploitaties { get; set; }
+    public DbSet<LnkGewassen> LnkGewassens { get; set; }
+    public DbSet<LnkGewassen1> LnkGewassen1s { get; set; }
+    public DbSet<LnkKunstmest> LnkKunstmests { get; set; }
+    public DbSet<LnkKunstmestGroepStreek> LnkKunstmestGroepStreeks { get; set; }
+    public DbSet<LnkMestDierPlaat> LnkMestDierPlaats { get; set; }
+    public DbSet<LnkMestTechniekPlaat> LnkMestTechniekPlaats { get; set; }
+    public DbSet<LnkMestToedieningsEmissy> LnkMestToedieningsEmissies { get; set; }
+    public DbSet<LnkStalDierSubCategorie> LnkStalDierSubCategories { get; set; }
+    public DbSet<LutCheckType> LutCheckTypes { get; set; }
+    public DbSet<LutDierCategorie> LutDierCategories { get; set; }
+    public DbSet<LutGewasGroep> LutGewasGroeps { get; set; }
+    public DbSet<LutKunstmestGroep> LutKunstmestGroeps { get; set; }
+    public DbSet<LutLandbouwStreek> LutLandbouwStreeks { get; set; }
+    public DbSet<LutMestType> LutMestTypes { get; set; }
+    public DbSet<LutMestverwerkingsTechniek> LutMestverwerkingsTechnieks { get; set; }
+    public DbSet<LutStalType> LutStalTypes { get; set; }
+    public DbSet<LutToedieningsPlaat> LutToedieningsPlaats { get; set; }
+    public DbSet<LutToedieningsTechniek> LutToedieningsTechnieks { get; set; }
+    public DbSet<MvwUitbatingActiviteit> MvwUitbatingActiviteits { get; set; }
+    public DbSet<MvwUitbatingAdressen> MvwUitbatingAdressens { get; set; }
+    public DbSet<SpatialRefSy> SpatialRefSies { get; set; }
+    public DbSet<Stallen> Stallens { get; set; }
+    public DbSet<TblCheck> TblChecks { get; set; }
+    public DbSet<TblConvenant> TblConvenants { get; set; }
+    public DbSet<TblDierSubCategorie> TblDierSubCategories { get; set; }
+    public DbSet<TblGebruiker> TblGebruikers { get; set; }
+    public DbSet<TblParameter> TblParameters { get; set; }
     public DbSet<TblPas> TblPas { get; set; }
-    public DbSet<TblStal> TblStal { get; set; }
-    public DbSet<TblVersie> TblVersie { get; set; }
-
+    public DbSet<TblPas1> TblPas1s { get; set; }
+    public DbSet<TblRegressie> TblRegressies { get; set; }
+    public DbSet<TblRegressieRechte> TblRegressieRechtes { get; set; }
+    public DbSet<TblStal> TblStals { get; set; }
+    public DbSet<TblStal1> TblStal1s { get; set; }
+    public DbSet<TblVersie> TblVersies { get; set; }
+    public DbSet<TblVersie1> TblVersie1s { get; set; }
+    public DbSet<VervoerMad> VervoerMads { get; set; }
+    public DbSet<Vlops1000mRooster> Vlops1000MRoosters { get; set; }
+    public DbSet<Vlops250mRooster> Vlops250MRoosters { get; set; }
+    
     public IlvoDbContext(DbContextOptions<IlvoDbContext> options) : base(options)
     {
     }
@@ -15,19 +78,18 @@ public class IlvoDbContext : DbContext
     // Override OnModelCreating if you need to configure your entity models
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        // Configure entity relationships, constraints, etc.
-        // For example:
-        // modelBuilder.Entity<Customer>()
-        //     .HasMany(c => c.Orders)
-        //     .WithOne(o => o.Customer)
-        //     .HasForeignKey(o => o.CustomerId);
     }
 
     // Optional: Override OnConfiguring if you want to provide configuration options
-    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    // {
-    //     // Configure database provider, connection string, etc.
-    // }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Configure database provider, connection string, etc.
+        if (!optionsBuilder.IsConfigured)
+        {
+            string connectionString = Constants.connectionString;
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
 
     // Custom Set method to retrieve DbSet dynamically
     public DbSet<TEntity> Set<TEntity>() where TEntity : class
--- ilvo_automatisation/Program.cs
+++ ilvo_automatisation/Program.cs
@@ -1,19 +1,29 @@
-﻿using ilvo_automatisation;
+﻿using ilvo_automatisation.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using ilvo_automatisation.Models;
+
+namespace ilvo_automatisation;
 
 public class Program
 {
     public static void Main(string[] args)
     {
         string connectionString = Constants.connectionString;
-        string outputPath = GetDefaultOutputPath(); // Get the default output path
+        // Get the default output path
+        string outputPath = GetDefaultOutputPath(); 
 
-        using (var dbContext = new IlvoDbContext(new DbContextOptionsBuilder<IlvoDbContext>().UseSqlServer(connectionString).Options))
-        {
-            GenerateClasses(dbContext, outputPath);
-        }
+        // Create the DbContext using the provided connection string
+        using var dbContext =
+            new IlvoDbContext(
+                new DbContextOptionsBuilder<IlvoDbContext>().UseSqlServer(connectionString).Options);
+        
+        // Add data to the DbContext
+        AddData(dbContext);
+
+        // Save the changes to the database
+        dbContext.SaveChanges();
+
+        // Generate classes and CSV files
+        GenerateClasses(dbContext, outputPath);
     }
 
     private static string GetDefaultOutputPath()
@@ -24,8 +34,82 @@ public class Program
         return defaultOutputPath;
     }
 
+    private static void AddData(IlvoDbContext dbContext)
+    {
+        // Create instances of the entity classes and add them to the respective DbSet properties
+
+        // Create a LutGewasGroep instance to use as the foreign key reference for LnkGewassen
+        var lutGewasGroep = new LutGewasGroep { Id = Guid.NewGuid() };
+        dbContext.LutGewasGroeps.Add(lutGewasGroep);
+
+        var lnkGewassen = new LnkGewassen
+        {
+            Id = Guid.NewGuid(),
+            CodeHoofdTeelt = 123,
+            GewasGroepId = lutGewasGroep.Id, // Assign the foreign key value from the lutGewasGroep instance
+            VersieId = Guid.NewGuid(),
+            OmsHoofdTeelt = "Some value",
+            GewasGroep = lutGewasGroep // Set the navigation property value
+        };
+        dbContext.LnkGewassens.Add(lnkGewassen);
+
+        // Create a TblVersie instance to use as the foreign key reference for TblPas
+        var tblVersie = new TblVersie { Id = Guid.NewGuid() };
+        dbContext.TblVersies.Add(tblVersie);
+
+        var tblPas = new TblPas
+        {
+            Id = Guid.NewGuid(),
+            Naam = "Some name",
+            ReductiePercentage = 0.5,
+            Passtal = true,
+            Pasvoeding = true,
+            Pasweide = false,
+            VersieId = tblVersie.Id // Assign the foreign key value from the tblVersie instance
+        };
+        dbContext.TblPas.Add(tblPas);
+
+        // Create a LutStalType instance to use as the foreign key reference for TblStal
+        var lutStalType = new LutStalType { Id = Guid.NewGuid() };
+        dbContext.LutStalTypes.Add(lutStalType);
+
+        var tblStal = new TblStal
+        {
+            Id = Guid.NewGuid(),
+            Naam = "Some name",
+            Omschrijving = "Some description",
+            FractieVloeibaar = 0.75,
+            ReductiePercentage = null, // Update if necessary
+            MestTypeId = Guid.NewGuid(),
+            StalTypeId = lutStalType.Id, // Assign the foreign key value from the lutStalType instance
+            VersieId = Guid.NewGuid(),
+            LnkStalDierSubCategories = new List<LnkStalDierSubCategorie>(), // Update if necessary
+            MestType = null!, // Update if necessary
+            StalType = lutStalType // Set the navigation property value
+        };
+        dbContext.TblStals.Add(tblStal);
+
+        // Create a TblGebruiker instance to use as the foreign key reference for TblVersie
+        var tblGebruiker = new TblGebruiker { Id = Guid.NewGuid() };
+        dbContext.TblGebruikers.Add(tblGebruiker);
+
+        var tblVersie2 = new TblVersie
+        {
+            Id = Guid.NewGuid(),
+            Naam = "Some name",
+            GebruikerId = tblGebruiker.Id, // Assign the foreign key value from the tblGebruiker instance
+            Publiek = true,
+            Gebruiker = tblGebruiker // Set the navigation property value
+        };
+        dbContext.TblVersies.Add(tblVersie2);
+
+        // Save the changes to the database
+        dbContext.SaveChanges();
+    }
+
     private static void GenerateClasses(IlvoDbContext dbContext, string outputPath)
     {
+        // Get the entity types from the DbContext's model
         var entityTypes = dbContext.Model.GetEntityTypes();
         var csvData = new List<string>();
 
@@ -33,6 +117,7 @@ public class Program
         {
             var className = entityType.Name;
 
+            // Generate the properties of the class based on entity properties
             var properties = entityType.GetProperties()
                 .Select(property => $"    public {property.ClrType} {property.Name} {{ get; set; }}")
                 .ToList();
@@ -42,60 +127,55 @@ public class Program
                                   string.Join("\n", properties) +
                                   "\n}\n";
 
-            var entityTypeClrType = entityType.ClrType;
+            // Retrieve records for each entity type
             var records1 = dbContext.Set<TblPas>().ToList();
-            var records2 = dbContext.Set<TblStal>();
+            var records2 = dbContext.Set<TblStal>().ToList();
             var records3 = dbContext.Set<TblVersie>().ToList();
             var records4 = dbContext.Set<LnkGewassen>().ToList();
 
+            // Add the headers for the CSV file
             csvData.Add(string.Join(",", entityType.GetProperties().Select(p => p.Name)));
 
-            foreach (var record in records1)
-            {
-                var propertyValues = entityType.GetProperties()
+            // Add the records for each entity type to the CSV data
+            csvData.AddRange(records1.Select(record => entityType.GetProperties()
                     .Select(property => GetValueString(record, property.Name))
-                    .ToList();
-                csvData.Add(string.Join(",", propertyValues));
-            }
+                    .ToList())
+                .Select(propertyValues => string.Join(",", propertyValues)));
 
-            foreach (var record in records2)
-            {
-                var propertyValues = entityType.GetProperties()
+            csvData.AddRange(records2.Select(record => entityType.GetProperties()
                     .Select(property => GetValueString(record, property.Name))
-                    .ToList();
-                csvData.Add(string.Join(",", propertyValues));
-            }
+                    .ToList())
+                .Select(propertyValues => string.Join(",", propertyValues)));
 
-            foreach (var record in records3)
-            {
-                var propertyValues = entityType.GetProperties()
+            csvData.AddRange(records3.Select(record => entityType.GetProperties()
                     .Select(property => GetValueString(record, property.Name))
-                    .ToList();
-                csvData.Add(string.Join(",", propertyValues));
-            }
+                    .ToList())
+                .Select(propertyValues => string.Join(",", propertyValues)));
 
-            foreach (var record in records4)
-            {
-                var propertyValues = entityType.GetProperties()
+            csvData.AddRange(records4.Select(record => entityType.GetProperties()
                     .Select(property => GetValueString(record, property.Name))
-                    .ToList();
-                csvData.Add(string.Join(",", propertyValues));
-            }
+                    .ToList())
+                .Select(propertyValues => string.Join(",", propertyValues)));
 
             // Add an empty row between data tables
             csvData.Add(string.Empty);
         }
 
+        // Create the CSV text by joining the CSV data
         var csvText = string.Join(Environment.NewLine, csvData);
 
         string outputFilePath = Path.Combine(outputPath, "DatabaseClasses.csv");
+        
+        // Write the CSV text to the output file
         File.WriteAllText(outputFilePath, csvText);
         Console.WriteLine($"CSV file generated successfully. Output file: {outputFilePath}");
     }
 
     private static string GetValueString(object obj, string propertyName)
     {
+        // Get the value of the specified property from the object
         var propertyValue = obj.GetType().GetProperty(propertyName)?.GetValue(obj);
+
         if (propertyValue == null)
             return "null";
 
