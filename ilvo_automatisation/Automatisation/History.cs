be3a4ee75bacc9ee728262eb6bb9eeb282fe74ae
Amy Norman <r0900514@student.vives.be>
2023-06-08 14:56:39 +0200

    deleted uneeded code

--- ilvo_automatisation/Automatisation/History.cs
+++ ilvo_automatisation/Automatisation/History.cs
@@ -19,60 +19,30 @@ namespace ilvo_automatisation.Automatisation
 
                     string historyTableName = $"history.{tableName}";
 
-                    // Controleer of de geschiedenistabel al bestaat
-                    bool historyTableExists;
-                    string checkTableExistsQuery = $@"
-                        IF OBJECT_ID('{historyTableName}', 'U') IS NOT NULL
-                            SELECT 1
-                        ELSE
-                            SELECT 0";
+                    string createHistoryTableQuery = $@"
+                        CREATE TABLE {historyTableName}(
+                            [HistoryID] [int] IDENTITY(1,1) NOT NULL,
+                            [ID] [uniqueidentifier] NOT NULL,
+                            [Naam] [nvarchar](50) NOT NULL,
+                            [GebruikerID] [uniqueidentifier] NOT NULL,
+                            [Publiek] [bit] NOT NULL,
+                            [UpdatedBy] [nvarchar](128) NULL,
+                            [UpdatedOn] [datetime] NULL,
+                            [Status] [nvarchar](30) NULL,
+                            PRIMARY KEY CLUSTERED ([HistoryID] ASC)
+                        )";
 
-                    using (SqlCommand checkTableExistsCommand = new SqlCommand(checkTableExistsQuery, connection))
+                    using (SqlCommand command = new SqlCommand(createHistoryTableQuery, connection))
                     {
-                        historyTableExists = (int)checkTableExistsCommand.ExecuteScalar() == 1;
+                        command.ExecuteNonQuery();
                     }
 
-                    if (historyTableExists)
-                    {
-                        // Voer een update-query uit op de bestaande geschiedenistabel
-                        string updateHistoryTableQuery = $@"
-                            ALTER TABLE {historyTableName}
-                            ADD [Status] [nvarchar](30) NULL";
-
-                        using (SqlCommand updateHistoryTableCommand = new SqlCommand(updateHistoryTableQuery, connection))
-                        {
-                            updateHistoryTableCommand.ExecuteNonQuery();
-                        }
-
-                        Console.WriteLine($"History table {historyTableName} updated successfully!");
-                    }
-                    else
-                    {
-                        // Maak een nieuwe geschiedenistabel aan
-                        string createHistoryTableQuery = $@"
-                            CREATE TABLE {historyTableName}(
-                                [HistoryID] [int] IDENTITY(1,1) NOT NULL,
-                                [ID] [uniqueidentifier] NOT NULL,
-                                [Naam] [nvarchar](50) NOT NULL,
-                                [GebruikerID] [uniqueidentifier] NOT NULL,
-                                [Publiek] [bit] NOT NULL,
-                                [UpdatedBy] [nvarchar](128) NULL,
-                                [UpdatedOn] [datetime] NULL,
-                                PRIMARY KEY CLUSTERED ([HistoryID] ASC)
-                            )";
-
-                        using (SqlCommand createHistoryTableCommand = new SqlCommand(createHistoryTableQuery, connection))
-                        {
-                            createHistoryTableCommand.ExecuteNonQuery();
-                        }
-
-                        Console.WriteLine($"History table {historyTableName} created successfully!");
-                    }
+                    Console.WriteLine($"History table {historyTableName} created successfully!");
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Error creating/updating history table: {ex.Message}");
+                Console.WriteLine($"Error creating history table: {ex.Message}");
             }
         }
     }
