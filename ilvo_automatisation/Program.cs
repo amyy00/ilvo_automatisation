415da8a0b8ce4570b7dd1d509d6f9a3addcdb022
sWeex720 <99887108+sWeex720@users.noreply.github.com>
2023-06-08 09:14:03 +0200

    program.cs correct zetten

--- ilvo_automatisation/Program.cs
+++ ilvo_automatisation/Program.cs
@@ -2,6 +2,7 @@
 using ilvo_automatisation.Data.Test;
 using ilvo_automatisation.Models;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace ilvo_automatisation
 {
@@ -10,14 +11,16 @@ namespace ilvo_automatisation
         public static void Main(string[] args)
         {
             string connectionString = Constants.connectionString;
-            
+
             // Create the DbContext using the provided connection string
-            using var dbContext = new EmavContext(new DbContextOptionsBuilder<EmavContext>().UseSqlServer(connectionString).Options);
+            using var dbContext =
+                new EmavContext(new DbContextOptionsBuilder<EmavContext>().UseSqlServer(connectionString).Options);
 
             // Get the default output path
             string outputPath = GetDefaultOutputPath();
 
-            Console.WriteLine("Please enter a command: csv, trigger, mockdata");
+            Begin: ;
+            Console.WriteLine("Please enter a command: csv, trigger, mock data, exit"); 
             string? input = Console.ReadLine();
 
             switch (input)
@@ -31,26 +34,33 @@ namespace ilvo_automatisation
                     generateCSV.GenerateFile(dbContext, outputPath);
                     Console.WriteLine("Program completed");
                     break;
-                
+
                 case "trigger":
                     Console.WriteLine("Executing command trigger...");
+                    //TODO add comments
                     TrackHistory.TestTrackHistory();
                     Console.WriteLine("Program completed");
                     break;
 
-                case "mockdata":
+                case "mock data":
                     Console.WriteLine("Executing command mockdata...");
                     //To fill the database with mock data
                     GenerateMockData.GenerateData();
                     Console.WriteLine("Program completed");
                     break;
 
+                case "exit":
+                    Console.WriteLine("Closing program...");
+                    Console.ReadLine();
+                    goto End;
+                    break;
+
                 default:
                     Console.WriteLine("Unknown command.");
                     break;
             }
-
-            Console.ReadLine();
+            goto Begin;
+            End:;
         }
 
         private static string GetDefaultOutputPath()
