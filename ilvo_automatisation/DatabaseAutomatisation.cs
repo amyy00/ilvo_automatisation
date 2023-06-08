23e4f00c0d731adb052c972ff0d05c23a16f53cd
sWeex720 <99887108+sWeex720@users.noreply.github.com>
2023-06-08 10:34:24 +0200

    push updates to main

--- ilvo_automatisation/DatabaseAutomatisation.cs
+++ ilvo_automatisation/DatabaseAutomatisation.cs
@@ -1,81 +1,89 @@
-﻿using Microsoft.Data.SqlClient;
-using System;
+﻿using ilvo_automatisation.Data;
+using ilvo_automatisation.Models;
+using Microsoft.Data.SqlClient;
 
 namespace ilvo_automatisation
 {
     public class DatabaseAutomatisation
     {
-        private SqlConnection connection;
-
-        public DatabaseAutomatisation(SqlConnection connection)
+        public static void AutomateTriggers<T>()
         {
-            this.connection = connection;
-        }
-
-        public void CreateOrUpdateTriggers()
-        {
-            CreateOrUpdateTrigger("tblStal");
-            CreateOrUpdateTrigger("tblPAS");
-            CreateOrUpdateTrigger("tblVersie");
-            CreateOrUpdateTrigger("lnkGewassen");
-        }
-
-        private void CreateOrUpdateTrigger(string tableName)
-        {
-            string triggerName = $"tbl{tableName}Trigger_DDL";
+            // Get the class name
+            string className = typeof(T).Name;
 
             try
             {
-                string createTriggerQuery = $@"
-                IF EXISTS (
-                    SELECT *
-                    FROM sys.triggers
-                    WHERE name = @triggerName
-                )
+                // Trigger for update
+                string updateTriggerQuery = $@"
+                Create TRIGGER [dbo].[{className}Trigger_UPDATE] ON [dbo].[{className}]
+                AFTER UPDATE AS
                 BEGIN
-                    -- Trigger already exists, modify it if needed
-                    -- Update the trigger code here
-                    ALTER TRIGGER {triggerName} ON DATABASE
-                    FOR CREATE_TABLE, ALTER_TABLE, DROP_TABLE
-                    AS
-                    BEGIN
-                        -- Trigger logic here
-                        -- Example: log the DDL changes in a separate table
-                        INSERT INTO DDLLogTable (EventTime, EventType, TableName)
-                        VALUES (GETDATE(), EVENTDATA().value('(/EVENT_INSTANCE/EventType)[1]', 'nvarchar(100)'), EVENTDATA().value('(/EVENT_INSTANCE/ObjectName)[1]', 'nvarchar(100)'))
-                    END
-                END
-                ELSE
-                BEGIN
-                    -- Trigger does not exist, create it
-                    -- Create the trigger code here
-                    DECLARE @createDDLTriggerQuery NVARCHAR(MAX) = N'
-                    CREATE TRIGGER ' + QUOTENAME(@triggerName) + '
-                    ON DATABASE
-                    FOR CREATE_TABLE, ALTER_TABLE, DROP_TABLE
-                    AS
-                    BEGIN
-                        -- Trigger logic here
-                        -- Example: log the DDL changes in a separate table
-                        INSERT INTO DDLLogTable (EventTime, EventType, TableName)
-                        VALUES (GETDATE(), EVENTDATA().value(''(/EVENT_INSTANCE/EventType)[1]'', ''nvarchar(100)''), EVENTDATA().value(''(/EVENT_INSTANCE/ObjectName)[1]'', ''nvarchar(100)''))
-                    END'
+                    SET NOCOUNT ON;
+                    DECLARE @ID uniqueidentifier
 
-                    EXEC sp_executesql @createDDLTriggerQuery, N'@triggerName NVARCHAR(100)', @triggerName = @triggerName
+                    SELECT @ID = dbo.{className}.ID
+                    FROM INSERTED, dbo.{className}
+
+                    INSERT INTO history.{className}( ID, UpdatedBy, UpdatedOn, status)
+                    VALUES( @ID, SUSER_SNAME(), GETDATE(), 'Updated')
                 END";
 
-                using (SqlCommand command = new SqlCommand(createTriggerQuery, connection))
+                using (SqlConnection connection = new SqlConnection(Constants.connectionString))
                 {
-                    command.Parameters.AddWithValue("@triggerName", triggerName);
-                    command.ExecuteNonQuery();
-                }
+                    connection.Open();
 
-                Console.WriteLine($"Trigger {triggerName} created or updated successfully!");
+                    using (SqlCommand command = new SqlCommand(updateTriggerQuery, connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                }
+                Console.WriteLine($"Trigger update {className.ToLower()} created successfully!");
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Error creating or updating trigger {triggerName}: {ex.Message}");
+                Console.WriteLine($"Trigger update {className.ToLower()}: " + ex.Message);
             }
+
+            try
+            {
+                // Trigger for delete
+                string deleteTriggerQuery = $@"
+                CREATE TRIGGER [dbo].[{className}Trigger_DELETE] ON [dbo].[{className}]
+                AFTER DELETE AS
+                BEGIN
+                    SET NOCOUNT ON;
+                    DECLARE @ID uniqueidentifier
+
+                    SELECT @ID = DELETED.ID
+                    FROM DELETED
+
+                    INSERT INTO history.{className}( ID, UpdatedBy, UpdatedOn, status)
+                    VALUES( @ID, SUSER_SNAME(), GETDATE(), 'Deleted')
+                END";
+
+                using (SqlConnection connection = new SqlConnection(Constants.connectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand(deleteTriggerQuery, connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                }
+                Console.WriteLine($"Triggers delete {className.ToLower()} created successfully!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Trigger delete {className.ToLower()}: " + ex.Message);
+            }
+        }
+
+        public static void TestTrackHistory()
+        {
+            AutomateTriggers<TblStal>();
+            AutomateTriggers<TblPas>();
+            AutomateTriggers<TblVersie>();
+            AutomateTriggers<LnkGewassen>();
         }
     }
 }
